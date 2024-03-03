a182a747271cd5e078d77dd8fe7dc3f5127ff69a
dovecoteescapee <dovecoteescapee@cock.email>
2024-03-03 16:26:58 +0300

    Update version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.dovecoteescapee.byedpi"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.0-rc1"
+        versionCode = 4
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
