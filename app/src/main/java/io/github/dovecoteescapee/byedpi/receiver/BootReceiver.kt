fb5f51c64dd738aa552c998fe2bb4eee27ba86c6
romanvht <romanvht@gmail.com>
2024-09-02 20:18:13 +0400

    Added check for activation of accessibility service before autostart

--- app/src/main/java/io/github/dovecoteescapee/byedpi/receiver/BootReceiver.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/receiver/BootReceiver.kt
@@ -4,13 +4,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import io.github.dovecoteescapee.byedpi.services.AutoStartAccessibilityService
+import io.github.dovecoteescapee.byedpi.utility.AccessibilityUtils
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
             intent.action == "android.intent.action.QUICKBOOT_POWERON") {
-            val serviceIntent = Intent(context, AutoStartAccessibilityService::class.java)
-            context.startService(serviceIntent)
+
+            val autorunEnabled = AccessibilityUtils.isAccessibilityServiceEnabled(
+                context,
+                AutoStartAccessibilityService::class.java
+            )
+
+            if(autorunEnabled) {
+                val serviceIntent = Intent(context, AutoStartAccessibilityService::class.java)
+                context.startService(serviceIntent)
+            }
         }
     }
 }
