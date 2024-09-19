a53646192650fea48b40a3ff411c9c0bd38630fd
romanvht <romanvht@gmail.com>
2024-09-19 14:17:13 +0400

    Add focus effect to selected app (for android TV)

--- app/src/main/java/io/github/dovecoteescapee/byedpi/fragments/AppSelectionFragment.kt
+++ app/src/main/java/io/github/dovecoteescapee/byedpi/fragments/AppSelectionFragment.kt
@@ -25,7 +25,7 @@ class AppSelectionFragment : Fragment() {
     private lateinit var adapter: AppSelectionAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_app_selection, container, false)
+        val view = inflater.inflate(R.layout.app_selection, container, false)
 
         recyclerView = view.findViewById(R.id.recyclerView)
         searchView = view.findViewById(R.id.searchView)
